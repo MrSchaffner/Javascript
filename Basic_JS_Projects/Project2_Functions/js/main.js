c1bc87100073334e68c4b7481c37e28072a58db1
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-02-09 14:12:21 -0800

    attempt 2

---
+++ Basic_JS_Projects/Project2_Functions/js/main.js
@@ -0,0 +1,24 @@
+
+    var message = "\"Hello Universe!\""
+    +" That's weird";
+
+    var multiple1 = "1", multiple2="2";
+    var expression = 3+5;
+    var concatenate = multiple1+multiple2;
+    
+    //alert(message);
+    /* or */
+    //window.alert(message);
+    /* or */
+    document.write(concatenate);
+
+    // a function that triggers when p tag is pressed.
+    function change(){
+document.getElementById("item1").innerHTML += "words"
+
+      }
+
+      // resets the content of the p tag
+      function reset(){
+        document.getElementById("item1").innerHTML = "GREAT RESET"
+      }
