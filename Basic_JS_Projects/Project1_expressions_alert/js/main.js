8669108d129f5704ea563d77abe170185dc17af4
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-02-09 13:06:41 -0800

    tests 1

---
+++ Basic_JS_Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,16 @@
+
+    var message = "\"Hello Universe!\""
+    +" That's weird";
+
+    var multiple1 = "1", multiple2="2";
+    var expression = 3+5;
+    
+    //alert(message);
+    /* or */
+    //window.alert(message);
+    /* or */
+    document.write(multiple2);
+
+    function change(){
+        document.getElementById("button1").innerHTML=message;
+    }
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Javascript
 
 Various projects created using one of the most popular languages, Javascript
+
+```bash
+<script>
+    alert("hello universe!");
+</script>
+```
