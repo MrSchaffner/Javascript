bb829e4ba4eb247f1ae9002dab2ee8e068c6ef07
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-02-09 18:31:05 -0800

    hello again

--- Basic_JS_Projects/Project5_comparison_type_coercion/js/main.js
+++ Basic_JS_Projects/Project5_comparison_type_coercion/js/main.js
@@ -16,7 +16,7 @@ function stringType(){
 }
 
 
-
+//squares the number, but it never displays infinity
 function squared(){
   var or = parseInt(document.getElementById("Number").innerHTML,10);
   document.getElementById("Number").innerHTML= or * or;
@@ -25,18 +25,23 @@ function squared(){
 function results(){
 var por = isNaN(5);
 var message =  
-"<br>is 5 Nan? " + isNaN(5) 
+
+"<br>" //NaN operators
++"<br>5+'5': " + (5+"5") 
++"<br>is 5 Nan? " + isNaN(5) 
 + "<br>is red Nan? " + isNaN("red")
 + "<br>0/0 is" +0/0
 + "<br>1E310 is displayed as: " + (1E310)
 + "<br>-1E310 is displayed as: " + (-1E310)
-+ "<br><br>3 is greater than 1,000: " + (3>1000)
++ "<br>" //boolean type operators
++ "<br>3 is greater than 1,000: " + (3>1000)
 + "<br>3 is exactly equal(===) to 3: " + (3===3)
 + "<br>3 is exactly equal to '4': " + (3==="4")
 + "<br>3 is exactly equal to '3': " + (3==='3')
 + "<br>3 is equal to(==) '3': also " + (3==='3')
 + "<br>3 is exactly equal to 4: " + (3===4)
-+ "<br><br>3>1 && 2>1 " + (3>1 && 2>1)
++ "<br>" //boolean
++ "<br>3>1 && 2>1 " + (3>1 && 2>1)
 + "<br>3>1 && 2<1 " + (3>1 && 2<1)
 + "<br>3>1 || 2>2 " + (3>1 || 2>2)
 + "<br>3<1 || 2<1 " + (3<1 || 2<1)
@@ -44,7 +49,7 @@ var message =
 + "<br>!3<1 " + (!3<1);
 
 document.getElementById("results").innerHTML=message;
-console.log(message);
+console.log(message); //all things displayed also appear in console
 console.log(1000000/1000000);
 console.log(3>1000);
 console.log(3==1000);
