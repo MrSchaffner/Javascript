62d0aa6b411ca28ef6ec7565bb1bae4f114f70f6
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-02-09 18:21:47 -0800

    type of added

--- Basic_JS_Projects/Project5_comparison_type_coercion/js/main.js
+++ Basic_JS_Projects/Project5_comparison_type_coercion/js/main.js
@@ -1,13 +1,7 @@
 
 //there is one function for each button.
 
-var Animal={
-  species:"dog",
-  sound:"bark",
-  sound2:"bark2"
-}
-
-delete Animal.sound2;
+document.write("The type of 'number' is: " + typeof "number" + "<br><br>")
 
 function whatType(){
   var or = 1;
@@ -30,8 +24,29 @@ function squared(){
 
 function results(){
 var por = isNaN(5);
-document.getElementById("results").innerHTML
-+= "<br>is 5 Nan? " + isNaN(5) 
+var message =  
+"<br>is 5 Nan? " + isNaN(5) 
 + "<br>is red Nan? " + isNaN("red")
-+ "<br>0/0 is" +0/0;
++ "<br>0/0 is" +0/0
++ "<br>1E310 is displayed as: " + (1E310)
++ "<br>-1E310 is displayed as: " + (-1E310)
++ "<br><br>3 is greater than 1,000: " + (3>1000)
++ "<br>3 is exactly equal(===) to 3: " + (3===3)
++ "<br>3 is exactly equal to '4': " + (3==="4")
++ "<br>3 is exactly equal to '3': " + (3==='3')
++ "<br>3 is equal to(==) '3': also " + (3==='3')
++ "<br>3 is exactly equal to 4: " + (3===4)
++ "<br><br>3>1 && 2>1 " + (3>1 && 2>1)
++ "<br>3>1 && 2<1 " + (3>1 && 2<1)
++ "<br>3>1 || 2>2 " + (3>1 || 2>2)
++ "<br>3<1 || 2<1 " + (3<1 || 2<1)
++ "<br>!3>1 " + (!3>1)
++ "<br>!3<1 " + (!3<1);
+
+document.getElementById("results").innerHTML=message;
+console.log(message);
+console.log(1000000/1000000);
+console.log(3>1000);
+console.log(3==1000);
+console.log(3==3);
 }
