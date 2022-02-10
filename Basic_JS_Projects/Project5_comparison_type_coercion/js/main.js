bfed4c9651744f5f91024c5c80df66bd4ba04395
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-02-09 17:56:27 -0800

    finally fixed it

--- Basic_JS_Projects/Project5_comparison_type_coercion/js/main.js
+++ Basic_JS_Projects/Project5_comparison_type_coercion/js/main.js
@@ -10,14 +10,28 @@ var Animal={
 delete Animal.sound2;
 
 function whatType(){
-  var or = document.getElementById("Math").innerHTML;
-  document.getElementById("Math").innerHTML= typeof or;
+  var or = 1;
+  document.getElementById("typer").innerHTML= typeof or;
 
 
 }
 
+function stringType(){
+  var or = "hello";
+  document.getElementById("typer").innerHTML= typeof or;
+}
+
+
 
 function squared(){
-  var or = parseInt(document.getElementById("Math").innerHTML,10);
-  document.getElementById("Math").innerHTML= or * or;
+  var or = parseInt(document.getElementById("Number").innerHTML,10);
+  document.getElementById("Number").innerHTML= or * or;
+}
+
+function results(){
+var por = isNaN(5);
+document.getElementById("results").innerHTML
++= "<br>is 5 Nan? " + isNaN(5) 
++ "<br>is red Nan? " + isNaN("red")
++ "<br>0/0 is" +0/0;
 }
